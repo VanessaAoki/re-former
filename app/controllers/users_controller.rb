d9681c255b3a61a2dfdb884742c5c52cdfbe7c96
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-04-15 17:09:30 -0300

    Fix the routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+
   def new 
     @user = User.new
   end
-
-  def edit 
-    @user = User.find([params[:id]])
-  end 
 
   def create
     #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
@@ -15,6 +15,19 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+  def edit 
+    @user = User.find([params[:id]])
+  end 
+
+  def update
+    @user = User.find(params[:id])    
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
     end
   end
 
